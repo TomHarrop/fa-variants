6f453999de7dd8e7470fee4cc706945b9119e9c3
Tom Harrop <twharrop@gmail.com>
2016-08-08 14:39:46 +0200

    generate flag files

--- fa-variants/__main__.py
+++ fa-variants/__main__.py
@@ -41,63 +41,16 @@ def main():
     # initialise pipeline
     main_pipeline = ruffus.Pipeline.pipelines["main"]
 
-    # test originate job
-    test_originate_files = ['ruffus/foo.txt', 'ruffus/bar.txt']
-    test_originate = main_pipeline.originate(
-        name='test_originate',
-        task_func=functions.generate_job_function(
-            job_script='src/test_originate',
-            job_name='test_originate',
-            job_type='originate'),
-        output=test_originate_files)
+    # bamfiles
+    raw_files = [x.name for x in os.scandir('data/bam') if
+                 x.name.endswith('.bam') and x.is_file]
+    raw_file_flags = [os.path.join("ruffus/", x) for x in raw_files]
+    mapped_raw = main_pipeline.originate(
+        name='mapped_raw',
+        task_func=functions.touch,
+        output=raw_file_flags)
 
-    # test download job
-    test_download = main_pipeline.originate(
-        name='test_download',
-        task_func=functions.generate_job_function(
-            job_script='src/test_download',
-            job_name='test_download',
-            job_type='download'),
-        output='ruffus/download.txt',
-        extras=[jgi_logon, jgi_password])
-
-    # test transform with multiple outputs (e.g. bamfile, FASTA etc)
-    test_transform = main_pipeline.transform(
-        name="test_transform",
-        task_func=functions.generate_job_function(
-            job_script='src/test_transform',
-            job_name='test_transform',
-            job_type='transform'),
-        input=test_originate,
-        filter=ruffus.suffix(".txt"),
-        output=["_transformed.txt", "_transformed.bam"])
-
-    # Transform ONLY the bam files produced by test_transform
-
-    # The filtering here is a bit crazy. `input` has to be an object, not
-    # ruffus.output_from(). `replace_inputs` should use `ruffus.inputs()` to
-    # match the files, but `filter` has to match the first file produced by
-    # the previous step, NOT necessarily the file that will be transformed!
-    test_selective_transform = main_pipeline.transform(
-        name="test_selective_transform",
-        task_func=functions.generate_job_function(
-            job_script='src/test_selective_transform',
-            job_name='test_selective_transform',
-            job_type='transform'),
-        input=test_transform,
-        replace_inputs=ruffus.inputs(r"\1.bam"),
-        filter=ruffus.suffix(".txt"),
-        output=".bof")
-
-    test_merge = main_pipeline.merge(
-        name='test_merge',
-        task_func=functions.generate_job_function(
-            job_script='src/test_merge',
-            job_name='test_merge',
-            job_type='merge'),
-        input=test_transform,
-        output='ruffus/foobar_merge.txt'
-        )
+    # mark duplicates with picard
 
     ###################
     # RUFFUS COMMANDS #
